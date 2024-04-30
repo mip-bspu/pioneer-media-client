d3b89c45958f3a0676b473b5aab982e52d2ef9be
RodionLV <arknim@mail.ru>
2024-04-30 17:40:08 +0900

    add: client

---
+++ src/client/index.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const KEY_TOKEN = "__pioneer-token"
+
+const client = axios.create({
+  baseURL: location.origin + "/api",
+  timeout: 60 * 1000,
+});
+
+client.interceptors.request.use(function (config) {
+  const token = localStorage.getItem(KEY_TOKEN)
+  config.headers.set(KEY_TOKEN, token)
+
+  return config;
+})
+
+export default client;
